85e0d9666df9d1cb300c55ff33ee8a81e58d1dd3
ranahusain <capthussain2point0@gmail.com>
2025-07-24 13:02:56 +0500

    strip payment implemented

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -18,6 +18,8 @@ import Playlist from "./pages/PlayList/PlayList";
 import MainPlayListPage from "./pages/MainPlayListPage/MainPlayListPage";
 import ArtistDetail from "./pages/ArtistDetail/ArtistDetail";
 import UserProfile from "./pages/UserProfile/UserProfile";
+import Payment from "./pages/Payment/Payment";
+import Completion from "./pages/Completion";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -33,6 +35,8 @@ const router = createBrowserRouter(
       <Route path="/Playlist/" element={<MainPlayListPage />} />
       <Route path="/artist/:artistname" element={<ArtistDetail />} />
       <Route path="/UserProfile" element={<UserProfile />} />
+      <Route path="/Payment" element={<Payment />} />
+      <Route path="/Completion" element={<Completion />} />
     </>
   )
 );
